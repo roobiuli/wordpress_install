e3ac10458e3ad91facddd88d3618fd0843829cd9
robert <nicoara.robert@gmail.com>
2016-11-27 04:19:25 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# wordpress_install
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'wordpress_install'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures wordpress_install'
+long_description 'Installs/Configures wordpress_install'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/wordpress_install/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/wordpress_install' if respond_to?(:source_url)
